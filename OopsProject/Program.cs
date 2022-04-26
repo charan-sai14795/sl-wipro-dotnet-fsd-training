0cd0e0f915ca542865a3daf7335386fa6d4584a0
sai charan <charansai5274@gmail.com>
2022-04-26 19:03:15 +0530

    just commited

--- OopsProject/Program.cs
+++ OopsProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
 namespace OopsProject
 {
     internal class Program
@@ -7,6 +8,16 @@ namespace OopsProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string oneclass = @"C:\Users\chara\Desktop\OneClass";
+            string student = @"C:\Users\chara\Desktop\Student";
+            string subject = @"C:\Users\chara\Desktop\Subject";
+            string []OneClass=File.ReadAllLines(oneclass);
+            string[] Student = File.ReadAllLines(student);
+            string[] Subject = File.ReadAllLines(subject);
+            List<OneClass>l1 = new List<OneClass>();
+            List<Student>l2 = new List<Student>();
+            List<Subject>l3 = new List<Subject>();
+
         }
     }
 }
---
+++ OopsProject/Subject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsProject
+{
+    internal class Subject
+    {
+    }
+}
